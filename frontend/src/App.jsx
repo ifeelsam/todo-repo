6ed847fb089e0860ff6176349f7b4068dc93213e
sam <sanskarsharma9005@gmail.com>
2024-10-16 02:46:44 +0530

    add frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,41 @@
-import {Button} from "@/components/ui/button"
+import { HoverEffect } from "./components/ui/hover-card";
+import {Input} from "./components/ui/input"
+import {Button } from "./components/ui/button"
+import { Plus, Trash2 } from 'lucide-react'
+
 export default function App() {
+  const addTodo = () => {}
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-      <Button variant="secondary">hellow</Button>
-    </h1>
-  )
+    <div className="max-w-5xl mx-auto px-8">
+      <div className="flex m-11">
+        <Input
+          type="text"
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Add a new todo"
+          className="flex-grow mr-2"
+        />
+        <Button onClick={addTodo}>
+          <Plus className="h-4 w-4 mr-2" />
+          Add
+        </Button>
+      </div>
+
+      <HoverEffect items={projects} />
+    </div>
+  );
 }
+export const projects = [
+  {
+    title: "go to gym",
+    description:
+      "40 reps chest || 50 reps pull"
+  },
+  {
+    title: "laudary",
+    description: "kapde dhone hai "
+  },
+  {
+    title: "study mathematics",
+    description: "exams are coming up study hard"
+  }
+];
